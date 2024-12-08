d04e790022fb43177f95b89e01725703f84cab68
“Akash <kolakkalakash@gmail.com>
2024-12-08 13:49:25 +0530

    reverted back changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 require('dotenv').config();
-const {registerUser, loginUser} = require('./controllers/userController')
 
-const PORT =  process.env.PORT;
+const userRoutes = require('./routes/userRoutes'); 
+
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
-app.post('/api/register', registerUser);
-app.post('/api/login', loginUser);
+app.use('/api', userRoutes);
 
-app.listen(PORT, 'localhost', ()=>{
+app.listen(PORT, 'localhost', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
