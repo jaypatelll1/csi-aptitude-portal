0582238421ab59433713fb9e7a9073576f096767
“Akash <kolakkalakash@gmail.com>
2024-12-08 13:32:00 +0530

    removed unwanted files

--- server/server.js
+++ server/server.js
@@ -1,29 +1,15 @@
-// const express = require('express');
-// const { query, initializeDB } = require('./db');
+const express = require('express');
+require('dotenv').config();
+const {registerUser, loginUser} = require('./controllers/userController')
 
-// const app = express();
-// app.use(express.json()); // Middleware for parsing JSON requests
+const PORT =  process.env.PORT;
 
-// // Initialize the database when the server starts
-// initializeDB();
+const app = express();
+app.use(express.json());
 
-// // Test route to insert and fetch data
-// app.post('/api/users', async (req, res) => {
-//   const { name, email } = req.body;
+app.post('/api/register', registerUser);
+app.post('/api/login', loginUser);
 
-//   try {
-//     // Insert data
-//     await query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
-    
-//     // Retrieve all users
-//     const result = await query('SELECT * FROM users');
-//     res.status(200).json({ success: true, users: result.rows });
-//   } catch (err) {
-//     console.error('Error:', err);
-//     res.status(500).json({ success: false, message: 'Database error' });
-//   }
-// });
-
-// // Start server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, 'localhost', ()=>{
+  console.log(`Server running on http://localhost:${PORT}`);
+});
