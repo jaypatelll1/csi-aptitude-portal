8d66ed56459285dab69b38963bd2e709c4a4f3f6
rk5507700 <rk5507700@gmail.com>
2024-12-21 11:54:14 +0530

    Added extra layer for verifying env variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 const { Pool } = require('pg');
+if(!process.env.PGDATABASE || !process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGPORT || !process.env.PGHOST){
+  console.error("Please set the environment variables PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD");
+  process.exit(1);
+}
 
 const pool = new Pool({
   host: process.env.PGHOST,
@@ -19,6 +23,7 @@ const query = async (text, params) => {
     return res;
   } catch (error) {
     console.error("Error running query!", error.stack);
+    throw error;
   } finally{
     if(client){
       client.release();
