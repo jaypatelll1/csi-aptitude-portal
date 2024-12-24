30b5087d2397b464bd06ffc4a4e99a7d3d774d49
“Akash <kolakkalakash@gmail.com>
2024-12-24 11:53:11 +0530

    test render commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,11 @@
-const express = require('express');
-require('dotenv').config();
-
-const userRoutes = require('./routes/userRoutes'); 
-
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 4000;
 
-const app = express();
-app.use(express.json());
-
-app.use('/api', userRoutes);
-
-app.get('/' , ()=> {
-    
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${port}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
