fc81c36ba9c122ed9387164138eadb68b1154c59
Bhavani <bhavani9271@gmail.com>
2024-12-25 20:17:26 +0530

    implemented draft, scheduled and past exams

--- server/routes/examRoutes.js
+++ server/routes/examRoutes.js
@@ -6,6 +6,11 @@ const {
   updateExam,
   deleteExam,
   getAllPaginatedExams,
+  getPaginatedDraftededExams,
+  getPaginatedPublishedExams,
+  getPaginatedPastExams,
+  publishExam,
+  markPastExam
 } = require('../controllers/examController');
 const { authorizeRoles } = require('../middlewares/roleAuthMiddleware');
 const { jwtAuthMiddleware } = require('../middlewares/jwtAuthMiddleware');
@@ -13,9 +18,17 @@ const { jwtAuthMiddleware } = require('../middlewares/jwtAuthMiddleware');
 const router = express.Router();
 
 router.post('/',jwtAuthMiddleware, authorizeRoles, createExam);
+
 router.get('/',jwtAuthMiddleware, getAllPaginatedExams); // Pagination
-router.get('/:exam_id',jwtAuthMiddleware,authorizeRoles, getExamById);
+router.get('/drafts', jwtAuthMiddleware,authorizeRoles, getPaginatedDraftededExams)
+router.get('/published', jwtAuthMiddleware, getPaginatedPublishedExams);
+router.get('/past', jwtAuthMiddleware, getPaginatedPastExams);
+router.get('/find/:exam_id',jwtAuthMiddleware, getExamById);
+
 router.put('/:exam_id',jwtAuthMiddleware, authorizeRoles, updateExam);
+router.put('/publish/:exam_id',jwtAuthMiddleware,authorizeRoles, publishExam); // to publish an exam
+router.put('/past-exam/:exam_id',jwtAuthMiddleware,authorizeRoles, markPastExam); // to mark an exam as past
+
 router.delete('/:exam_id', jwtAuthMiddleware,authorizeRoles, deleteExam);
 
 module.exports = router;
---
+++ server/utils/autoUpdateExamStatus.js
@@ -0,0 +1,24 @@
+const cron = require('node-cron');
+const pool = require('../config/db');
+
+// Run this job every hour
+const autoUpdate = cron.schedule('* * * * *', async () => {
+  try {
+    const now = new Date();
+    const result = await pool.query(
+      `UPDATE exams SET status = 'past' WHERE status = 'scheduled' AND end_time < $1`,
+      [now]
+    );
+    console.log(`${result.rowCount} exams updated to 'past'.`);
+  } catch (error) {
+    console.error('Error updating exam statuses:', error);
+  }
+});
+
+// Handle SIGINT for graceful shutdown
+process.on('SIGINT', async () => {
+  console.log('Stopping cron jobs...');
+  autoUpdate.stop(); // Stop cron job
+  console.log('Exiting process.');
+  process.exit();
+});
