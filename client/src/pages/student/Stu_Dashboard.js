5517c499d8f883ea7ae9b5472c6732dca3e5eb50
jay patel <pateljay8886@gmail.com>
2025-01-02 14:50:17 +0530

    Enhance login functionality with password visibility toggle; refactor student dashboard component imports and structure

--- client/src/pages/student/Stu_Dashboard.js
+++ client/src/pages/student/Stu_Dashboard.js
@@ -1,12 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import MSidebar from "../../components/student/home/MSidebar";
-import Stu_TestCard from "../../components/student/home/Stu_TestCard";
-import Stu_PastTestCard from "../../components/student/home/Stu_PastTestCard";
+import StuTestCard from "../../components/student/home/Stu_TestCard";
+import StuPastTestCard from "../../components/student/home/Stu_PastTestCard";
 import Details from "../../components/student/home/Stu_Details";
-import axios  from "axios";
-import { useLocation } from 'react-router-dom';
-
-
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 const pastTests = [
   {
@@ -53,28 +51,18 @@ const pastTests = [
   },
 ];
 
-
 function StudentDashboard() {
-
   const location = useLocation();
   const userData = location.state?.userData;
   // console.log('uers data is',userData );
 
-
-  
   const [tests, setTests] = useState([]);
   const [filter, setFilter] = useState("all");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [isDetailsOpen, setIsDetailsOpen] = useState(false);
   const detailsRef = useRef(null);
 
-  
-  
-
   const fetchTests = async (filterType) => {
-    setLoading(true);
-    setError(null);
+
     let url = "/api/exams/live"; // Default for "All"
 
     if (filterType === "upcoming") {
@@ -85,25 +73,19 @@ function StudentDashboard() {
 
     try {
       const response = await axios.get(url);
-      
+
       setTests(response.data.exams || []);
-      // console.log('test is ', tests);
-      
     } catch (err) {
-      setError("Failed to fetch tests. Please try again later.");
     } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchTests(filter)
+    fetchTests(filter);
   }, [filter]);
-
 
   const openDetails = () => setIsDetailsOpen(true);
   const closeDetails = () => setIsDetailsOpen(false);
-  
 
   useEffect(() => {
     // Close the Details component when clicking outside
@@ -125,57 +107,85 @@ function StudentDashboard() {
     setFilter(e.target.value); // Update filter
   };
 
-
   return (
     <div className="flex h-screen">
       <MSidebar />
-      <div id="main-section" className='ml-64 flex-grow bg-white h-max overflow-hidden'>
-        <div className='bg-gray-100 h-14 border-b border-gray-200 flex items-center'>
+      <div
+        id="main-section"
+        className="ml-64 flex-grow bg-white h-max overflow-hidden"
+      >
+        <div className="bg-gray-100 h-14 border-b border-gray-200 flex items-center">
           <h1 className="text-xl font-medium text-gray-800 ml-5">Dashboard</h1>
-          <div className="h-9 w-9 rounded-full bg-blue-300 ml-auto mr-5 flex items-center justify-center text-blue-700 text-sm hover:cursor-pointer" onClick={openDetails}>AM</div>
+          <div
+            className="h-9 w-9 rounded-full bg-blue-300 ml-auto mr-5 flex items-center justify-center text-blue-700 text-sm hover:cursor-pointer"
+            onClick={openDetails}
+          >
+            AM
+          </div>
           <div ref={detailsRef}>
-          {isDetailsOpen && <Details name={userData.name} email={userData.email}  mobile={userData.phone} branch={userData.branch} />}</div>
+            {isDetailsOpen && (
+              <Details
+                name={userData.name}
+                email={userData.email}
+                mobile={userData.phone}
+                branch={userData.branch}
+              />
+            )}
+          </div>
         </div>
-        <h1 className="text-blue-700 text-2xl ml-10 mt-10 font-medium">Welcome to Atharva college Aptitude Portal</h1>
+        <h1 className="text-blue-700 text-2xl ml-10 mt-10 font-medium">
+          Welcome to Atharva college Aptitude Portal
+        </h1>
         <div className="flex border-b border-gray-200 mx-10 mt-5 pb-3 items-center">
-          <select className="bg-white px-3 py-1 focus:outline-none font-medium text-black hover:cursor-pointer" value={filter}
-            onChange={handleFilterChange} >
+          <select
+            className="bg-white px-3 py-1 focus:outline-none font-medium text-black hover:cursor-pointer"
+            value={filter}
+            onChange={handleFilterChange}
+          >
             <option value="all">All tests</option>
             <option value="upcoming">Upcoming</option>
             <option value="past">Past</option>
           </select>
-          <h1 className="font-semibold text-blue-700 text-xs ml-auto mr-3 hover:cursor-pointer">view all</h1>
+          <h1 className="font-semibold text-blue-700 text-xs ml-auto mr-3 hover:cursor-pointer">
+            view all
+          </h1>
         </div>
         <div className="grid-cols-3 gap-5 grid  mx-10 mt-5 px-5">
-        {tests.length > 0 ? (
-        tests.map((test, index) => (
-          <Stu_TestCard 
-            key={test.exam_id || index}  // Using exam.id if available, otherwise index
-            examId={test.exam_id }
-            testName={test.exam_name}
-            duration={test.duration}
-            lastDate={null} 
-          />
-        ))
-      ) : (
-        <p>No exams available.</p>
-      )}
+          {tests.length > 0 ? (
+            tests.map((test, index) => (
+              <StuTestCard
+                key={test.exam_id || index} // Using exam.id if available, otherwise index
+                examId={test.exam_id}
+                testName={test.exam_name}
+                duration={test.duration}
+                lastDate={null}
+              />
+            ))
+          ) : (
+            <p>No exams available.</p>
+          )}
         </div>
         <div className="flex border-b border-gray-200 mx-10 mt-5 pb-3 items-center">
-          <h1 className="font-semibold text-black text-lg ml-3 mt-5">Analytics</h1>
+          <h1 className="font-semibold text-black text-lg ml-3 mt-5">
+            Analytics
+          </h1>
         </div>
-        
-        <div className=" mx-10 mt-5 flex overflow-x-auto space-x-4 pb-3" 
-    style={{ scrollbarWidth: 'none' }}>
+
+        <div
+          className=" mx-10 mt-5 flex overflow-x-auto space-x-4 pb-3"
+          style={{ scrollbarWidth: "none" }}
+        >
           {pastTests.map((test, index) => (
             <div className="flex-shrink-0">
-            <Stu_PastTestCard 
-              key={index}
-              testName={test.testName}
-              submittedOn={test.submittedOn}
-              time={test.time}
-              marks={test.marks}
-              status={test.status} /></div>
+              <StuPastTestCard
+                key={index}
+                testName={test.testName}
+                submittedOn={test.submittedOn}
+                time={test.time}
+                marks={test.marks}
+                status={test.status}
+              />
+            </div>
           ))}
         </div>
       </div>
