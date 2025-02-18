7d2c37a171e2824ab6dbfc77470c84559b6303ee
Siddesh Navthale <Siddesh755@users.noreply.github.com>
2025-02-18 22:38:39 +0530

    Add analytics display components and integrate data fetching in Stu_Analytics

---
+++ client/src/components/analytics/DisplayComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DisplayComponent = ({ title, value }) => {
+  return (
+    <div className=" flex flex-col items-center border-2 border-gray-400 rounded-lg p-6 size-48 max-h-40 mb-2  ">
+      <h2 className="text-lg font-semibold mb-2 ">{title}</h2>
+      <span className="text-6xl font-bold text-[#1349C5] ">{value}</span>
+    </div>
+  );
+};
+
+export default DisplayComponent;
