ee8485e1495f35e9543011b7d4e3f323581e7124
A1YAARY <harshpoojary10b@gmail.com>
2025-02-19 16:37:46 +0530

    changes in cron job

--- server/utils/autoUpdateExamStatus.js
+++ server/utils/autoUpdateExamStatus.js
@@ -17,20 +17,6 @@ const autoUpdate = cron.schedule('*/5 * * * *', async () => {
 
     if (result.rows.length > 0) {
       console.log(`${result.rows.length} exams updated to 'past'.`);
-
-      // Process each updated exam_id
-      for (const row of result.rows) {
-        console.log(`Processing results for Exam ID: ${row.exam_id}`);
-        try {
-          await calculateAndStoreTotalScore(row.exam_id); // Trigger result creation for the updated exam
-          console.log(` results processed for Exam ID: ${row.exam_id}`);
-        } catch (error) {
-          console.error(
-            `Error processing results for Exam ID: ${row.exam_id}:`,
-            error
-          );
-        }
-      }
     } else {
       console.log('No exams were updated.');
     }
