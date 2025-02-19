19f385751a233f7cf1c70fab2c03f95ff2cb0256
Aditi Yadav <aditiyadav2405@gmail.com>
2025-02-19 12:49:36 +0530

    added PerformanceComponent.js

--- client/src/pages/department/Dep_Analytics.js
+++ client/src/pages/department/Dep_Analytics.js
@@ -1,11 +1,13 @@
 import React from "react";
 import DepartmentRanking from "../../components/analytics/DepartmentRanking"; 
+import PerformanceComponent from "../../components/analytics/PerformanceComponent";
 
 function Dep_Analytics() {
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">Department Analytics</h1>
       <DepartmentRanking />
+      <PerformanceComponent/>
     </div>
   );
 }
