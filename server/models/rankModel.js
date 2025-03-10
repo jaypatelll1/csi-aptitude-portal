db6e1d25be677869247171655c5ed0f4f22fe107
Bhavani Murali <bhavani9271@gmail.com>
2025-03-10 17:39:26 +0530

    overall rank and dept rank

--- server/models/rankModel.js
+++ server/models/rankModel.js
@@ -7,10 +7,11 @@ const generateRank = async () => {
           student_name,
           department_name AS department,
           SUM(total_score) AS total_score,
-          RANK() OVER (PARTITION BY department_name ORDER BY SUM(total_score) DESC) AS rank
+          RANK() OVER (ORDER BY SUM(total_score) DESC) AS overall_rank
+          RANK() OVER (PARTITION BY department_name ORDER BY SUM(total_score) DESC) AS department_rank,
         FROM student_analysis
         GROUP BY student_id, student_name, department_name
-        ORDER BY department_name, rank;
+        ORDER BY department_name, department_rank, overall_rank;
       `;
     let results = await query(queryText);
     results = results.rows;
@@ -19,12 +20,12 @@ const generateRank = async () => {
     await query(queryText);
 
     results.map(async (ranking) => {
-      let { student_id, student_name, department, rank, total_score } = ranking;
+      let { student_id, student_name, department, total_score, overall_rank,  department_rank } = ranking;
 
-      queryText = `INSERT INTO student_rank (student_id, student_name, department_name, rank, total_score)
-        VALUES ($1, $2, $3, $4, $5);`;
+      queryText = `INSERT INTO student_rank (student_id, student_name, department_name, total_score, overall_rank, department_rank)
+        VALUES ($1, $2, $3, $4, $5, $6);`;
 
-      await query(queryText, [student_id, student_name, department, rank, total_score]);
+      await query(queryText, [student_id, student_name, department, total_score, overall_rank, department_rank]);
     });
     return results.rows;
   } catch (error) {
