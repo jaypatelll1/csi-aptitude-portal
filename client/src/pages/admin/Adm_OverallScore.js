32d1bd3694cd699b6434af3248f3e293cc4f6f6a
A1YAARY <harshpoojary10b@gmail.com>
2025-07-12 20:27:36 +0530

    fix: update API URL to use environment variable for backend base URL

--- client/src/pages/admin/Adm_OverallScore.js
+++ client/src/pages/admin/Adm_OverallScore.js
@@ -35,8 +35,9 @@ function Adm_OverallScore() {
     try {
       setLoading(true);
       setError(null);
+    let API_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL;
       
-      const url = "http://localhost:4000/api/analysis/overallAnalysis";
+      const url = `${API_BASE_URL}api/analysis/overallAnalysis`;
       const response = await axios.get(url, { withCredentials: true });
       
       // Access the response data directly
