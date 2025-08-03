d3cd5b8c040e517b65c9da8faf82f921bec6c9ee
A1YAARY <harshpoojary10b@gmail.com>
2025-08-04 01:26:17 +0530

    feat: enable SSL connection for PostgreSQL database

--- server/config/db.js
+++ server/config/db.js
@@ -15,7 +15,9 @@ const pool = new Pool({
   user: process.env.PGUSER, // fixed key
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl: true,
+ ssl: {
+    rejectUnauthorized: false
+  },
    max: 50,                  // limit clients in pool
       idleTimeoutMillis: 30000, // disconnect idle clients
 });
